40da412c9ae844cba1fd3f5d68a2e49bdbf9a742
Sumit <sk7627577@gmail.com>
2024-02-05 12:15:24 +0530

    adding color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        clrBlack: "#000000",
+        clrBlue: "#0000ee",
+        clrSkyBlue: "#116dff",
+        clrDarkGreen: "#1d2927",
+        clrLightGreen: "#014235",
+        clrWhite: "#ffffff",
+        clrLightOrange: "#faf5eb",
+        clrLightGray: "#e8e6e6",
+      },
+    },
   },
   plugins: [],
-}
-
+};
