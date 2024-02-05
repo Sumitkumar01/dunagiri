864bbe58dafbf53028020cc9714ecc1276d70b13
Hem Bhadur <sushilkc2611@gmail.com>
2024-02-05 12:49:32 +0530

    commit init

---
+++ src/components/Header/Nav_List.js
@@ -0,0 +1,37 @@
+const Nav_Lists = [
+  {
+    linkName: "Home",
+    src: "/",
+  },
+  {
+    linkName: "Accomondation",
+    src: "/",
+  },
+  {
+    linkName: "Retreats",
+    src: "/",
+    subMenu: [
+      "self guide exploration",
+      "self guide exploration",
+      "self guide exploration",
+    ],
+  },
+  {
+    linkName: "Activities",
+    src: "/",
+  },
+  {
+    linkName: "Gallary",
+    src: "/",
+  },
+  {
+    linkName: "blog",
+    src: "/",
+  },
+  {
+    linkName: "contact us",
+    src: "/",
+  },
+];
+
+export default Nav_Lists;
