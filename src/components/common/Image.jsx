89417d3462a9863c10593ab3335ee29ede701f98
Sumit <sk7627577@gmail.com>
2024-02-05 11:49:34 +0530

    tailwind setup

---
+++ src/components/common/Image.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Image() {
+  return (
+    <>
+      <img
+        decoding="async"
+        width={855}
+        height={473}
+        src="https://fielmente.com/wp-content/uploads/2023/07/Sachin.png"
+        data-src="https://fielmente.com/wp-content/uploads/2023/07/Sachin.png"
+        className="lazy attachment-large size-large wp-image-11108 entered loaded two_elementor_element"
+        alt="Fielmente"
+        data-srcset="https://fielmente.com/wp-content/uploads/2023/07/Sachin.png 855w,
+https://fielmente.com/wp-content/uploads/2023/07/Sachin-300x166.png 300w,
+https://fielmente.com/wp-content/uploads/2023/07/Sachin-768x425.png 768w,
+https://fielmente.com/wp-content/uploads/2023/07/Sachin-600x332.png 600w"
+        data-sizes="(max-width: 855px) 100vw, 855px"
+        data-ll-status="loaded"
+        sizes="(max-width: 855px) 100vw, 855px"
+        srcSet="https://fielmente.com/wp-content/uploads/2023/07/Sachin.png 855w,
+https://fielmente.com/wp-content/uploads/2023/07/Sachin-300x166.png 300w,
+https://fielmente.com/wp-content/uploads/2023/07/Sachin-768x425.png 768w,
+https://fielmente.com/wp-content/uploads/2023/07/Sachin-600x332.png 600w"
+      />
+    </>
+  );
+}
+
+export default Image;
